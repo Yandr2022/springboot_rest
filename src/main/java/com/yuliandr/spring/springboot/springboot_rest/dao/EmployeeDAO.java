c2b418cfae26cf3efbe51090f539021833fea86b
Yandr2022 <sharkievich@gmail.com>
2023-03-20 10:06:52 +0300

    Initial commit

---
+++ src/main/java/com/yuliandr/spring/springboot/springboot_rest/dao/EmployeeDAO.java
@@ -0,0 +1,14 @@
+package com.yuliandr.spring.springboot.springboot_rest.dao;
+
+import com.yuliandr.spring.springboot.springboot_rest.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeDAO {
+
+    public List<Employee>getAllEmployees();
+    public void saveEmployee(Employee employee);
+    public Employee getEmployee(int id);
+    public void deleteEmployee(int id);
+
+}
